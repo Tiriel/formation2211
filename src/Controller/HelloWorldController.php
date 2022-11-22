67ee49bb95b934aa28236c6f596f120f034da58e
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-11-22 16:38:16 +0100

    Fix controller

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -9,16 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloWorldController extends AbstractController
 {
-    /*
-
-    /hello?name=Adrien
-    /hello/Adrien (/hello doit continuer à fonctionner)
-    */
-    #[Route('/hello', name: 'app_hello_world')]
-    public function index(Request $request): Response
+    #[Route('/hello/{name}', name: 'app_hello_world')]
+    public function index(Request $request, string $name = 'World'): Response
     {
-        $name = $request->query->get('name') ?? 'World';
-
         return $this->render('hello_world/index.html.twig', [
             'controller_name' => $name,
         ]);
