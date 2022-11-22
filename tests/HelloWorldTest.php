a251247648d8717d54d1578736f78e59e4d9711f
Adrien LUCAS <adrien.lucas@sensiolabs.com>
2022-11-22 16:37:50 +0100

    First test

---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HelloWorldTest extends WebTestCase
+{
+    public function provideHelloCases(): array
+    {
+        return [
+            ['/ジョン・ドウ', 'ジョン・ドウ'],
+            ['/Adrien', 'Adrien'],
+            ['', 'World']
+        ];
+    }
+
+
+    /**
+     * @dataProvider provideHelloCases
+     */
+    public function testItSaysHello(string $urlSuffix, string $expectedName): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/users'.$urlSuffix);
+
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello '.$expectedName);
+    }
+}
